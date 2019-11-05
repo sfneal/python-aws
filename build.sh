17a1f622cf5c0d4f1496757e999e0b95bd51016e
mrstephenneal <stephen.neal14@gmail.com>
2019-11-05 14:57:05 -0500

    FIX build and push scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t stephenneal/python-aws:alpine-v1 ./alpine-v1/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build -t stephenneal/python-aws:alpine-v1 "${DIR}"/alpine-v1/
--- push.sh
+++ push.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-sh ./build.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sh "${DIR}"/build.sh
 
 docker push stephenneal/python-aws:alpine-v1
