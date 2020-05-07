e289cabd598683b6e51bad2ab30aa6e1b99217fc
mrstephenneal <stephen.neal14@gmail.com>
2020-05-07 19:06:07 -0400

    FIX build & push scripts docker image names

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t stephenneal/python-aws:alpine-v1 "${DIR}"/alpine-v1/
+docker build -t stephenneal/python-aws:3.7-alpine-v1 "${DIR}"/3.7-alpine-v1/
+docker build -t stephenneal/python-aws:3.8-alpine-v1 "${DIR}"/3.8-alpine-v1/
--- push.sh
+++ push.sh
@@ -4,4 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sh "${DIR}"/build.sh
 
-docker push stephenneal/python-aws:alpine-v1
+docker push stephenneal/python-aws:3.7-alpine-v1
+docker push stephenneal/python-aws:3.8-alpine-v1
