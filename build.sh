729e2e7301f17418322f0ad91a1a2e997efa7cc8
Stephen Neal <stephen.neal14@gmail.com>
2021-01-04 12:26:27 -0500

    OPTIMIZE Travis CI config by adding env matrix & param passing to build.sh script
    FIX build.sh to accept arguments.

--- build.sh
+++ build.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
+# Base directory containing source code
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t stephenneal/python-aws:3.7-alpine-v1 "${DIR}"/3.7-alpine-v1/
-docker build -t stephenneal/python-aws:3.8-alpine-v1 "${DIR}"/3.8-alpine-v1/
-docker build -t stephenneal/python-aws:3.8-alpine-v2 "${DIR}"/3.8-alpine-v2/
-docker build -t stephenneal/python-aws:3.9-alpine "${DIR}"/3.9-alpine/
-docker build -t stephenneal/python-aws:3.9-buster "${DIR}"/3.9-buster/
-docker build -t stephenneal/python-aws:3.10-rc-alpine "${DIR}"/3.10-rc-alpine/
-docker build -t stephenneal/python-aws:3.10-rc-buster "${DIR}"/3.10-rc-buster/
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
+
+# Check if the TAG variable is set
+if [ -z "$TAG" ]
+
+  # Only build one image
+  then
+    docker build -t stephenneal/python-aws:"${TAG}" "${DIR}"/"${TAG}"/
+
+  # Build all images
+  else
+    docker build -t stephenneal/python-aws:3.7-alpine-v1 "${DIR}"/3.7-alpine-v1/
+    docker build -t stephenneal/python-aws:3.8-alpine-v1 "${DIR}"/3.8-alpine-v1/
+    docker build -t stephenneal/python-aws:3.8-alpine-v2 "${DIR}"/3.8-alpine-v2/
+    docker build -t stephenneal/python-aws:3.9-alpine "${DIR}"/3.9-alpine/
+    docker build -t stephenneal/python-aws:3.9-buster "${DIR}"/3.9-buster/
+    docker build -t stephenneal/python-aws:3.10-rc-alpine "${DIR}"/3.10-rc-alpine/
+    docker build -t stephenneal/python-aws:3.10-rc-buster "${DIR}"/3.10-rc-buster/
+fi
