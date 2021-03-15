35d33d768d26d30c78111edc8f21c2477ce4367d
Stephen Neal <stephen.neal14@gmail.com>
2021-03-15 12:51:43 -0400

    ADD 'deploy' section to Travis CI config for pushing Docker images to Docker Hub

--- push.sh
+++ push.sh
@@ -2,12 +2,26 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sh "${DIR}"/build.sh
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
 
-docker push stephenneal/python-aws:3.7-alpine-v1
-docker push stephenneal/python-aws:3.8-alpine-v1
-docker push stephenneal/python-aws:3.8-alpine-v2
-docker push stephenneal/python-aws:3.9-alpine
-docker push stephenneal/python-aws:3.9-buster
-docker push stephenneal/python-aws:3.10-rc-alpine
-docker push stephenneal/python-aws:3.10-rc-buster
+# Check if the TAG variable is set
+if [ "$TAG" != null ]
+
+  # Only build & push one image
+  then
+    sh "${DIR}"/build.sh "${TAG}"
+    docker push stephenneal/python-aws:"${TAG}"
+
+  # Build & push all images
+  else
+    sh "${DIR}"/build.sh
+
+    docker push stephenneal/python-aws:3.7-alpine-v1
+    docker push stephenneal/python-aws:3.8-alpine-v1
+    docker push stephenneal/python-aws:3.8-alpine-v2
+    docker push stephenneal/python-aws:3.9-alpine
+    docker push stephenneal/python-aws:3.9-buster
+    docker push stephenneal/python-aws:3.10-rc-alpine
+    docker push stephenneal/python-aws:3.10-rc-buster
+fi
