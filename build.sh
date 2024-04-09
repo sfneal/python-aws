d0a8157777cbcab8028bc12b8da552795a2dbbac
sfneal <stephen.neal14@gmail.com>
2024-04-09 13:49:27 -0400

    CUT 3.7 images from build/push scripts

--- build.sh
+++ build.sh
@@ -15,7 +15,6 @@ if [ "$TAG" != null ]
 
   # Build all images
   else
-    docker build -t stephenneal/python-aws:3.7-alpine-v1 "${DIR}"/3.7-alpine-v1/
     docker build -t stephenneal/python-aws:3.8-alpine-v1 "${DIR}"/3.8-alpine-v1/
     docker build -t stephenneal/python-aws:3.8-alpine-v2 "${DIR}"/3.8-alpine-v2/
     docker build -t stephenneal/python-aws:3.9-alpine "${DIR}"/3.9-alpine/
